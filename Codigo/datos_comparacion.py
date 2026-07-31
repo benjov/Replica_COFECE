e9b18fe8b49842064708c0b0469f2733f7c83aec
Victor Camps Abarca <victorcamps70@gmail.com>
2026-07-31 12:24:42 -0600

    Comparación con 2014 añandida

--- Codigo/datos_comparacion.py
+++ Codigo/datos_comparacion.py
@@ -56,3 +56,145 @@ T_V_PAPER = {
     "Trans. aéreo": 4.368,
     "Autobús foráneo": 2.718,
 }
+
+B_0_2014 = {
+    "Tortillas": 0.52970,
+    "Pan": -0.25048,
+    "Pollo+Huevo": 0.56392,
+    "Carne res": 0.12424,
+    "Carnes proc.": 0.07958,
+    "Lácteos": 0.12160,
+    "Frutas": -0.08811,
+    "Verduras": -0.18100,
+    "Bebidas": -1.11144,
+    "Medicamentos": 0.10168,
+    "Transporte": -0.71873,
+}
+
+B_1_2014 = {
+    "Tortillas": -0.68323,
+    "Pan": 0.15292,
+    "Pollo+Huevo": -0.58730,
+    "Carne res": 0.25934,
+    "Carnes proc.": 0.04058,
+    "Lácteos": 0.20564,
+    "Frutas": 0.36257,
+    "Verduras": 0.63866,
+    "Bebidas": 2.25812,
+    "Medicamentos": 0.01461,
+    "Transporte": 1.43925,
+}
+
+ELASTICIDADES_POR_CATEGORIA_2014 = {
+    "Tortillas": 1.091,
+    "Pan": 1.313,
+    "Pollo+Huevo": 1.066,
+    "Carne res": 1.102,
+    "Carnes proc.": 1.062,
+    "Lácteos": 1.030,
+    "Frutas": 1.049,
+    "Verduras": 1.034,
+    "Bebidas": 1.154,
+    "Medicamentos": 1.033,
+    "Materiales": 0.981,
+    "Trans. aéreo": 0.925,
+    "Autobús foráneo": 1.211,
+}
+
+ELASTICIDADES_POR_REGION_2014 = {
+    "Noroeste": 1.118,
+    "Noreste": 1.111,
+    "Oeste": 1.102,
+    "Este": 1.102,
+    "Centro Norte": 1.116,
+    "Centro Sur": 1.101,
+    "Suroeste": 1.110,
+    "Sureste": 1.110,
+}
+
+PRECIOS_POR_CATEGORIA_2014 = {
+    "Tortillas": 13.53,
+    "Pan": 4.21,
+    "Pollo+Huevo": 36.87,
+    "Carne res": 108.40,
+    "Carnes proc.": 95.16,
+    "Lácteos": 48.10,
+    "Frutas": 21.74,
+    "Verduras": 16.23,
+    "Bebidas": 12.29,
+    "Medicamentos": 213.65,
+    "Transporte": 143.76,
+    "Materiales": 109.03,
+    "Trans. aéreo": 145.56,
+    "Autobús foráneo": 143.11,
+}
+
+BETA_N_2014 = {
+    "Tortillas": 1.019,
+    "Pan": 1.019,
+    "Pollo+Huevo": 0.984,
+    "Carne res": 0.993,
+    "Carnes proc.": 0.873,
+    "Lácteos": 0.889,
+    "Frutas": 1.048,
+    "Verduras": 0.847,
+    "Bebidas": 0.544,
+    "Medicamentos": 0.940,
+    "Transporte foráneo": 0.661,
+    "Materiales": 0.716,
+    "Trans. aéreo": 0.107,
+    "Autobús foráneo": 0.084,
+}
+T_V_2014 = {
+    "Tortillas": 216.293,
+    "Pan": 57.626,
+    "Pollo+Huevo": 154.076,
+    "Carne res": 46.138,
+    "Carnes proc.": 56.755,
+    "Lácteos": 72.893,
+    "Frutas": 249.782,
+    "Verduras": 50.453,
+    "Bebidas": 46.370,
+    "Medicamentos": 126.846,
+    "Transporte foráneo": 39.024,
+    "Materiales": 94.574,
+    "Trans. aéreo": 24.367,
+    "Autobús foráneo": 8.298,
+}
+
+SOBREPRECIOS_PAPER = {
+    "Frutas": 238.52,
+    "Pan": 199.95,
+    "Materiales": 113.25,
+    "Lácteos": 95.43,
+    "Verduras": 30.47,
+    "Trans. aéreo": 27.40,
+    "Tortillas": 26.19,
+    "Autobús foráneo": 14.54,
+    "Carne res": 8.13,
+    "Pollo+Huevo": 14.02,
+    "Bebidas": 4.85,
+    "Medicamentos": 4.36,
+    "Carnes proc.": 1.86,
+}
+
+SOBREPRECIOS_2014 = {
+    "Tortillas": 400.00,
+    "Pan": 292.90,
+    "Pollo+Huevo": 400.00,
+    "Carne res": 400.00,
+    "Carnes proc.": 397.11,
+    "Lácteos": 400.00,
+    "Frutas": 398.17,
+    "Verduras": 387.47,
+    "Bebidas": 87.01,
+    "Medicamentos": 400.00,
+    "Materiales": 270.31,
+    "Trans. aéreo": 18.67,
+    "Autobús foráneo": 9.55,
+}
+
+PAPER_M = [841, 1097, 1286, 1410, 1487, 1613, 1738, 1907, 2052, 2237, 1497]
+PAPER_P = [30.9, 23.6, 21.4, 18.9, 16.7, 15.1, 13.6, 11.9, 9.5, 5.7, 15.7]
+REPLICA_M = [2356, 2883, 3378, 3570, 3752, 4150, 4291, 4634, 4791, 5934, 3974]
+REPLICA_P = [28.3, 20.7, 18.5, 16.0, 14.0, 13.0, 11.2, 9.6, 7.4, 4.8, 14.3]
