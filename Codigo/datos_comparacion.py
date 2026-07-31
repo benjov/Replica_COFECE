4ca3ca2824f6ac748f28fb995ed95e6a218602c4
Victor Camps Abarca <victorcamps70@gmail.com>
2026-07-30 19:28:26 -0600

    Codigo limpiado hasta parte 7

---
+++ Codigo/datos_comparacion.py
@@ -0,0 +1,58 @@
+PAPER_ELASTICIDADES = {
+    "Tortillas": 1.054,
+    "Pan": 1.462,
+    "Pollo+Huevo": 1.261,
+    "Carne res": 0.735,
+    "Carnes proc.": 0.968,
+    "Lácteos": 1.289,
+    "Frutas": 1.415,
+    "Verduras": 1.389,
+    "Bebidas": 1.110,
+    "Medicamentos": 0.943,
+    "Transporte foráneo": 0.847,
+    "Transporte aéreo": 1.246,
+    "Materiales constr.": 0.934,
+}
+
+ELASTICIDADES_POR_REGION = {
+    "Noroeste": 1.232,
+    "Noreste": 1.171,
+    "Oeste": 1.240,
+    "Este": 1.237,
+    "Centro Norte": 1.209,
+    "Centro Sur": 1.168,
+    "Suroeste": 1.179,
+    "Sureste": 1.165,
+}
+
+BETA_N_PAPER = {
+    "Tortillas": 0.183,
+    "Pan": 1.477,
+    "Pollo+Huevo": 0.139,
+    "Carne res": 0.047,
+    "Carnes proc.": 0.017,
+    "Lácteos": 0.626,
+    "Frutas": 1.120,
+    "Verduras": 0.328,
+    "Bebidas": 0.047,
+    "Medicamentos": 0.026,
+    "Materiales": 0.493,
+    "Trans. aéreo": 0.196,
+    "Autobús foráneo": 0.081,
+}
+
+T_V_PAPER = {
+    "Tortillas": 3.223,
+    "Pan": 16.268,
+    "Pollo+Huevo": 1.796,
+    "Carne res": 2.851,
+    "Carnes proc.": 0.906,
+    "Lácteos": 3.933,
+    "Frutas": 12.033,
+    "Verduras": 3.249,
+    "Bebidas": 1.531,
+    "Medicamentos": 1.566,
+    "Materiales": 5.535,
+    "Trans. aéreo": 4.368,
+    "Autobús foráneo": 2.718,
+}
